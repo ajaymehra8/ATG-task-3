aa9505cc529f0871dbff7758f26e829e3226a510
Ajay Mehra <ajaymehra4492@gmail.com>
2024-06-22 21:56:18 +0530

    Adding responsive code

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -5,13 +5,21 @@ import UserInfo from "../component/UserInfo";
 import { UseChange } from "../context/StateProvider";
 
 const Homepage = () => {
-    const {selectedUser}=UseChange();
+  const { selectedUser } = UseChange();
   return (
     <Box display={"flex"} width={"100%"} height={"100vh"}>
-      <Box width={"40%"} height={"100%"} display={{base:selectedUser?"none":"initial"}}>
+      <Box
+        width={{ xs: selectedUser ? "0" : "100%", md: "40%" }}
+        height={"100%"}
+        display={{ xs: selectedUser ? "none" : "initial", md: "initial" }}
+      >
         <Users />
       </Box>
-      <Box width={"60%"} height={"100%"} display={{base:selectedUser?"initial":"none"}}>
+      <Box
+        width={{ xs: selectedUser ? "100%" : "0", md: "60%" }}
+        height={"100%"}
+        display={{ xs: selectedUser ? "initial" : "none", md: "initial" }}
+      >
         <UserInfo />
       </Box>
     </Box>
